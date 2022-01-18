62553da1d6d0f8c3448f04895aa21285fda14f47
Papiya338 <papiya180074@diit.info>
2022-01-19 01:10:10 +0600

    "implementation of admin tamplate"

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.dashboard')
+@section('content')
+@include('layouts.nav')
+<div class="container">
+    <div class="row">
+        <h1>BLOG Page</h1>
+    </div>
+</div>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
 
 @section('content')
+@include('layouts.nav')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
